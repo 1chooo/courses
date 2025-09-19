53ea42ebca915d205ac67f5193aeace215a55cd9
Hugo (Chun-Ho) Lin <hugo970217@gmail.com>
2025-09-19 08:15:35 -0700

    feat: add csci 455 lab 4
    Co-authored-by: junhanzhangjoey <junhanzhangjoey@users.noreply.github.com>

---
+++ csci-455/lab/lab-04/Nums.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+
+/**
+ * Stores a sequence of integer data values and supports some computations
+ * with it.
+ * 
+ * CS 455 Lab 4.
+ */
+public class Nums {
+  private ArrayList<Integer> values;
+
+  /**
+   * Create an empty sequence of nums.
+   */
+  public Nums() {
+    values = new ArrayList<Integer>();
+  }
+
+  /**
+   * Add a value to the end of the sequence.
+   */
+  public void add(int value) {
+    values.add(value);
+  }
+
+  /**
+   * Return the minimum value in the sequence.
+   * If the sequence is empty, returns Integer.MAX_VALUE
+   */
+  public int minVal() {
+
+    if (values.size() == 0) {
+      return Integer.MAX_VALUE;
+    }
+
+    int min = values.get(0);
+
+    for (int i = 1; i < values.size(); i++) {
+      if (values.get(i) < min) {
+        min = values.get(i);
+      }
+    }
+
+    return min;
+  }
+
+  /**
+   * Prints out the sequence of values as a space-separated list
+   * on one line surrounded by parentheses.
+   * Does not print a newline.
+   * E.g., "(3 7 4 10 2 7)", for empty sequence: "()"
+   */
+  public void printVals() {
+    System.out.print("(");
+    for (int i = 0; i < values.size(); i++) {
+      System.out.print(values.get(i));
+      if (i < values.size() - 1) {
+        System.out.print(" ");
+      }
+    }
+    System.out.print(")");
+  }
+
+  /**
+   * Returns a new Nums object with all the values from this Nums
+   * object that are above the given threshold. The values in the
+   * new object are in the same order as in this one.
+   * E.g.: call to myNums.valuesGT(10) where myNums = (3 7 19 4 21 19 10)
+   * returns (19 21 19)
+   * myNums after call: (3 7 19 4 21 19 10)
+   * The method does not modify the object the method is called on.
+   */
+  public Nums valuesGT(int threshold) {
+    Nums result = new Nums();
+
+    for (int i = 0; i < values.size(); i++) {
+      if (values.get(i) > threshold) {
+        result.add(values.get(i));
+      }
+    }
+
+    return result;
+  }
+}
---
+++ csci-455/lab/lab-04/ex1/Nums.java
@@ -0,0 +1,69 @@
+package ex1;
+
+import java.util.ArrayList;
+
+/**
+ * Stores a sequence of integer data values and supports some computations
+ * with it.
+ * 
+ * CS 455 Lab 4.
+ */
+public class Nums {
+  private ArrayList<Integer> values;
+  /**
+   * Create an empty sequence of nums.
+   */
+  public Nums() {
+    values = new ArrayList<Integer>();
+  }
+
+  /**
+   * Add a value to the end of the sequence.
+   */
+  public void add(int value) {
+    values.add(value);
+  }
+
+  /**
+   * Return the minimum value in the sequence.
+   * If the sequence is empty, returns Integer.MAX_VALUE
+   */
+  public int minVal() {
+
+    return 0; // stub code to get it to compile
+
+  }
+
+  /**
+   * Prints out the sequence of values as a space-separated list
+   * on one line surrounded by parentheses.
+   * Does not print a newline.
+   * E.g., "(3 7 4 10 2 7)", for empty sequence: "()"
+   */
+  public void printVals() {
+    System.out.print("(");
+    for (int i = 0; i < values.size(); i++) {
+      System.out.print(values.get(i));
+      if (i < values.size() - 1) {
+        System.out.print(" ");
+      }
+    }
+    System.out.print(")");
+  }
+
+  /**
+   * Returns a new Nums object with all the values from this Nums
+   * object that are above the given threshold. The values in the
+   * new object are in the same order as in this one.
+   * E.g.: call to myNums.valuesGT(10) where myNums = (3 7 19 4 21 19 10)
+   * returns (19 21 19)
+   * myNums after call: (3 7 19 4 21 19 10)
+   * The method does not modify the object the method is called on.
+   */
+  public Nums valuesGT(int threshold) {
+
+    return new Nums(); // stub code to get it to compile
+
+  }
+
+}
